a2b400250d0e0d5eec236315325ad1e23eef7d14
Steven Tan <git@sktan.com>
2020-05-06 08:36:53 +0000

    Added HTTP requests handler for PacificES Cloud server library

--- pacifices/cloud/server.py
+++ pacifices/cloud/server.py
@@ -4,6 +4,7 @@ A class to handle management of https://pacifices.cloud servers
 import typing
 import requests
 import json
+import urllib
 
 class server(object):
   """ A class to handle management of https://pacifices.cloud servers """
@@ -20,7 +21,7 @@ class server(object):
     """
     self.__api_key = api_key
   
-  def __api_endpoint(self, route):
+  def __api_endpoint(self, route: str):
     r"""Return the API endpoint relevant to the action we are performing
 
     Args:
@@ -29,7 +30,57 @@ class server(object):
     Returns:
       str: A fully built URI e.g. https://api.pacifices.cloud/v1/servers
     """
-    return "{:s}{:s}".format(self.__api_url_base, route)
+    return "{:s}{:s}".format(self.__api_url_base, route.strip("/"))
+
+  def __api_request(self, route: str, data: typing.Optional[dict]=None, http_type: str="GET"):
+    r"""Sends an API request to the specified route using the given data parameter
+
+    Args:
+      route      (str): The route of the action we are looking to perform e.g. servers
+      data      (dict): The data sent with the the API request
+      http_type  (str): The type of HTTP request to send (GET, POST, DELETE, PUT)
+    
+    Returns:
+      dict: Data returned from the API consisting of the JSON response
+    
+    Examples:
+      >>> self.__api_request("servers/abcd-efgh-ijkl", http_type: "DELETE")
+      {
+        "success": true,
+        "error": false,
+        "result": null
+      }
+    """
+    endpoint = self.__api_endpoint(route)
+    headers = {
+      'Authorization': self.__api_key
+    }
+    if http_type == "GET":
+      if data:
+        request_params = urllib.parse.urlencode(data)
+        endpoint = "{:s}?{:s}".format(endpoint, request_params)
+      response = requests.get(
+        endpoint,
+        headers=headers
+      )
+    elif http_type == "POST":
+      response = requests.post(
+        endpoint,
+        headers=headers,
+        data=data
+      )
+    elif http_type == "POST":
+      response = requests.post(
+        endpoint,
+        headers=headers,
+        data=data
+      )
+    elif http_type == "GET":
+      response = requests.delete(
+        endpoint,
+        headers=headers
+      )
+    return response.json()
 
   def create(self, name: str,
     password: typing.Optional[str]=None,
